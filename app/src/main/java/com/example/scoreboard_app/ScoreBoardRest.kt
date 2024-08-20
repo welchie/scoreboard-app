1974b114e1b66c1b4cca8778ca210dc8608cd40a
Chris Welch <welchie99@gmail.com>
2024-08-20 08:25:26 +0100

    Updated Rest class

--- app/src/main/java/com/example/scoreboard_app/ScoreBoardRest.kt
+++ app/src/main/java/com/example/scoreboard_app/ScoreBoardRest.kt
@@ -18,6 +18,83 @@ class ScoreBoardRest {
         print(obj["numActivities"])
         print(obj["teamLogo"])
 
+
+        /**
+         * [
+         *   {
+         *     "teamName": "Sharks",
+         *     "score": 176,
+         *     "numActivities": 4,
+         *     "teamLogo": "2131492905"
+         *   },
+         *   {
+         *     "teamName": "Owls",
+         *     "score": 20,
+         *     "numActivities": 4,
+         *     "teamLogo": "2131492887"
+         *   },
+         *   {
+         *     "teamName": "Elephants",
+         *     "score": 10,
+         *     "numActivities": 1,
+         *     "teamLogo": "2131492868"
+         *   },
+         *   {
+         *     "teamName": "Bees",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492865"
+         *   },
+         *   {
+         *     "teamName": "Foxes",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492874"
+         *   },
+         *   {
+         *     "teamName": "Pigs",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492896"
+         *   },
+         *   {
+         *     "teamName": "Lions",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492884"
+         *   },
+         *   {
+         *     "teamName": "Rays",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492899"
+         *   },
+         *   {
+         *     "teamName": "Fish",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492871"
+         *   },
+         *   {
+         *     "teamName": "Pandas",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492890"
+         *   },
+         *   {
+         *     "teamName": "Hippos",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492880"
+         *   },
+         *   {
+         *     "teamName": "Zebras",
+         *     "score": 0,
+         *     "numActivities": 0,
+         *     "teamLogo": "2131492908"
+         *   }
+         * ]
+         */
     }
 
 
